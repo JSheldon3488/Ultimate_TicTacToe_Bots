274a07d0c1d61340af69ce918493e35ea917bfea
Justin Sheldon <JSheldon3488@gmail.com>
2020-03-20 20:06:47 +0000

    Fixed Spacing between boards on Clicks

--- src/controller.cpp
+++ src/controller.cpp
@@ -28,17 +28,32 @@ std::map<std::string, int> Controller::HandleInput() {
             break;
         }
         else if (e.type == SDL_MOUSEBUTTONDOWN) {
+            //Factor in additional spacing for x and y due to gaps between games
+            int adjusted_x = e.button.x;
+            int adjusted_y = e.button.y;
+            // Adjust x coordinate
+            if (e.button.x > tile_width*6+40) {
+                adjusted_x -= 40;
+            }
+            else if (e.button.x > tile_width*3+20) {
+                adjusted_x -= 20;
+            }
+            // Adjust y coordinate
+            if (e.button.y > tile_height*6+40) {
+                adjusted_y -= 40;
+            }
+            else if (e.button.y > tile_height*3+20) {
+                adjusted_y -= 20;
+            }
+
             //Convert coordinates to a game #, row in that game, and column in that game.
             // Solve for game #
-            int game_col = e.button.x/(3*tile_width);
-            int game_row = e.button.y/(3*tile_height);
-            click_result["x"] = e.button.x;
-            click_result["y"] = e.button.y;
+            int game_col = adjusted_x/(3*tile_width);
+            int game_row = adjusted_y/(3*tile_height);
             click_result["board"] = (3*game_row + game_col);
-            // Solve for row
-            click_result["row"] = (e.button.y - 3*tile_height*game_row)/tile_height;
-            click_result["col"] = (e.button.x - 3*tile_width*game_col)/tile_width;
-            // Solve for col
+            // Solve for row and col
+            click_result["row"] = (adjusted_y - 3*tile_height*game_row)/tile_height;
+            click_result["col"] = (adjusted_x - 3*tile_width*game_col)/tile_width;      
         }
     }
     return click_result;
